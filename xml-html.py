6ddd1866736026958b6f77d6d7005a2c4e4e64d9
root <root@localhost.localdomain>
2018-04-23 14:28:42 +0100

    commit

---
+++ xml-html.py
@@ -0,0 +1,28 @@
+# !/usr/bin/python
+import subprocess
+import os
+import re
+import nmap
+pattern = "\d+"
+path ="./"
+files = os.listdir(path)
+
+for file in files:
+    pattern = re.compile(pattern)
+    x=re.findall(pattern,file)
+    if x :
+        xml=file
+        html=x[0]+".html"
+
+	subprocess.call(['xsltproc',xml,'-o',html])
+        
+print("------------------------------SUCESSSSS----------------------------------------------------")
+
+
+
+
+
+
+
+
+
